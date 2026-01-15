13ba6ae6eade1a588303e78648e4614173edce16
Coolchickenguy <coolchickenguy@protonmail.com>
2026-01-14 17:20:39 -0700

    Update jimp usage for 1.x.x

--- src/proxy/skins/ImageEditor.ts
+++ src/proxy/skins/ImageEditor.ts
@@ -19,34 +19,85 @@ export namespace ImageEditor {
     if (native) sharp = (await import("sharp")).default;
     else {
       try {
-        Jimp = (await import("jimp" as any)).default;
+        Jimp = {
+          ...(await import("jimp" as any)),
+          // Replace the default constructor with a custom one
+          Jimp: class CustomBitmapConstructor extends (await import("jimp" as any)).Jimp {
+            constructor(bitmap) {
+              if (
+                bitmap &&
+                bitmap.width != null &&
+                bitmap.height != null &&
+                bitmap.data != null
+              ) {
+                super({
+                  data: bitmap.data,
+                  width: bitmap.width,
+                  height: bitmap.height,
+                });
+              } else {
+                super(bitmap);
+              }
+            }
+          }
+        };
       } catch (err) {
         const logger = new Logger("ImageEditor.js");
         logger.fatal("**** ERROR: UNABLE TO LOAD JIMP!");
-        logger.fatal("Please ensure that Jimp is installed by running 'npm install jimp' in the terminal.");
-        logger.fatal("If you'd like to use the faster native image editor, please set the 'useNatives' option in the config to true.");
+        logger.fatal(
+          "Please ensure that Jimp is installed by running 'npm install jimp' in the terminal.",
+        );
+        logger.fatal(
+          "If you'd like to use the faster native image editor, please set the 'useNatives' option in the config to true.",
+        );
         logger.fatal(`Error: ${err.stack}`);
         process.exit(1);
       }
-      Jimp.appendConstructorOption(
-        "Custom Bitmap Constructor",
-        (args) => args[0] && args[0].width != null && args[0].height != null && args[0].data != null,
-        (res, rej, args) => {
-          this.bitmap = args[0];
-          res();
-        }
-      );
     }
 
     loadedLibraries = true;
   }
 
-  export async function copyRawPixelsJS(imageIn: Jimp, imageOut: Jimp, dx1: number, dy1: number, dx2: number, dy2: number, sx1: number, sy1: number, sx2: number, sy2: number): Promise<Jimp> {
-    console.log(imageOut);
+  export async function copyRawPixelsJS(
+    imageIn: Jimp,
+    imageOut: Jimp,
+    dx1: number,
+    dy1: number,
+    dx2: number,
+    dy2: number,
+    sx1: number,
+    sy1: number,
+    sx2: number,
+    sy2: number,
+  ): Promise<Jimp> {
     if (dx1 > dx2) {
-      return _copyRawPixelsJS(imageIn, imageOut, sx1, sy1, dx2, dy1, sx2 - sx1, sy2 - sy1, imageIn.getWidth(), imageOut.getWidth(), true);
+      return _copyRawPixelsJS(
+        imageIn,
+        imageOut,
+        sx1,
+        sy1,
+        dx2,
+        dy1,
+        sx2 - sx1,
+        sy2 - sy1,
+        imageIn.width,
+        imageOut.width,
+        true,
+      );
     } else {
-      return _copyRawPixelsJS(imageIn, imageOut, sx1, sy1, dx2, dy1, sx2 - sx1, sy2 - sy1, imageIn.getWidth(), imageOut.getWidth(), false);
+      return _copyRawPixelsJS(
+        imageIn,
+        imageOut,
+        sx1,
+        sy1,
+        dx2,
+        dy1,
+        sx2 - sx1,
+        sy2 - sy1,
+        imageIn.width,
+        imageOut.width,
+        false,
+      );
     }
   }
 
@@ -80,7 +131,19 @@ export namespace ImageEditor {
   //   // });
   // }
 
-  async function _copyRawPixelsJS(imageIn: Jimp, imageOut: Jimp, srcX: number, srcY: number, dstX: number, dstY: number, width: number, height: number, imgSrcWidth: number, imgDstWidth: number, flip: boolean) {
+  async function _copyRawPixelsJS(
+    imageIn: Jimp,
+    imageOut: Jimp,
+    srcX: number,
+    srcY: number,
+    dstX: number,
+    dstY: number,
+    width: number,
+    height: number,
+    imgSrcWidth: number,
+    imgDstWidth: number,
+    flip: boolean,
+  ) {
     for (let y = 0; y < height; y++) {
       for (let x = 0; x < width; x++) {
         let srcIndex = (srcY + y) * imgSrcWidth + srcX + x;
@@ -92,25 +155,76 @@ export namespace ImageEditor {
         const pixelColor = imageIn.getPixelColor(srcX + x, srcY + y);
         const rgba = Jimp.intToRGBA(pixelColor);
 
-        imageOut.setPixelColor(Jimp.rgbaToInt(rgba.r, rgba.g, rgba.b, rgba.a), dstX + x, dstY + y);
+        imageOut.setPixelColor(
+          Jimp.rgbaToInt(rgba.r, rgba.g, rgba.b, rgba.a),
+          dstX + x,
+          dstY + y,
+        );
       }
     }
 
     return imageOut;
   }
 
-  export async function copyRawPixels(imageIn: Sharp, imageOut: Sharp, dx1: number, dy1: number, dx2: number, dy2: number, sx1: number, sy1: number, sx2: number, sy2: number): Promise<Sharp> {
+  export async function copyRawPixels(
+    imageIn: Sharp,
+    imageOut: Sharp,
+    dx1: number,
+    dy1: number,
+    dx2: number,
+    dy2: number,
+    sx1: number,
+    sy1: number,
+    sx2: number,
+    sy2: number,
+  ): Promise<Sharp> {
     const inMeta = await imageIn.metadata(),
       outMeta = await imageOut.metadata();
 
     if (dx1 > dx2) {
-      return _copyRawPixels(imageIn, imageOut, sx1, sy1, dx2, dy1, sx2 - sx1, sy2 - sy1, inMeta.width!, outMeta.width!, true);
+      return _copyRawPixels(
+        imageIn,
+        imageOut,
+        sx1,
+        sy1,
+        dx2,
+        dy1,
+        sx2 - sx1,
+        sy2 - sy1,
+        inMeta.width!,
+        outMeta.width!,
+        true,
+      );
     } else {
-      return _copyRawPixels(imageIn, imageOut, sx1, sy1, dx1, dy1, sx2 - sx1, sy2 - sy1, inMeta.width!, outMeta.width!, false);
+      return _copyRawPixels(
+        imageIn,
+        imageOut,
+        sx1,
+        sy1,
+        dx1,
+        dy1,
+        sx2 - sx1,
+        sy2 - sy1,
+        inMeta.width!,
+        outMeta.width!,
+        false,
+      );
     }
   }
 
-  async function _copyRawPixels(imageIn: Sharp, imageOut: Sharp, srcX: number, srcY: number, dstX: number, dstY: number, width: number, height: number, imgSrcWidth: number, imgDstWidth: number, flip: boolean): Promise<Sharp> {
+  async function _copyRawPixels(
+    imageIn: Sharp,
+    imageOut: Sharp,
+    srcX: number,
+    srcY: number,
+    dstX: number,
+    dstY: number,
+    width: number,
+    height: number,
+    imgSrcWidth: number,
+    imgDstWidth: number,
+    flip: boolean,
+  ): Promise<Sharp> {
     const inData = await imageIn.raw().toBuffer();
     const outData = await imageOut.raw().toBuffer();
     const outMeta = await imageOut.metadata();
@@ -140,31 +254,167 @@ export namespace ImageEditor {
     });
   }
 
-  export async function toEaglerSkinJS(image: Buffer): Promise<Util.BoundedBuffer<typeof Constants.EAGLERCRAFT_SKIN_CUSTOM_LENGTH>> {
-    let jimpImage = await Jimp.read(image),
-      height = jimpImage.getHeight();
+  export async function toEaglerSkinJS(
+    image: Buffer,
+  ): Promise<
+    Util.BoundedBuffer<typeof Constants.EAGLERCRAFT_SKIN_CUSTOM_LENGTH>
+  > {
+    let jimpImage = await Jimp.Jimp.read(image),
+      height = jimpImage.height;
     if (height != 64) {
       // assume 32 height skin
-      let imageOut = await Jimp.create(64, 64, 0x0);
+      let imageOut = new Jimp.Jimp({width: 64, height: 4, color: 0x0});
 
-      for (let x = 0; x < jimpImage.getWidth(); x++) {
-        for (let y = 0; y < jimpImage.getHeight(); y++) {
+      for (let x = 0; x < jimpImage.width; x++) {
+        for (let y = 0; y < jimpImage.height; y++) {
           imageOut.setPixelColor(jimpImage.getPixelColor(x, y), x, y);
         }
       }
 
-      imageOut = await copyRawPixelsJS(jimpImage, imageOut, 24, 48, 20, 52, 4, 16, 8, 20);
-      imageOut = await copyRawPixelsJS(jimpImage, imageOut, 28, 48, 24, 52, 8, 16, 12, 20);
-      imageOut = await copyRawPixelsJS(jimpImage, imageOut, 20, 52, 16, 64, 8, 20, 12, 32);
-      imageOut = await copyRawPixelsJS(jimpImage, imageOut, 24, 52, 20, 64, 4, 20, 8, 32);
-      imageOut = await copyRawPixelsJS(jimpImage, imageOut, 28, 52, 24, 64, 0, 20, 4, 32);
-      imageOut = await copyRawPixelsJS(jimpImage, imageOut, 32, 52, 28, 64, 12, 20, 16, 32);
-      imageOut = await copyRawPixelsJS(jimpImage, imageOut, 40, 48, 36, 52, 44, 16, 48, 20);
-      imageOut = await copyRawPixelsJS(jimpImage, imageOut, 44, 48, 40, 52, 48, 16, 52, 20);
-      imageOut = await copyRawPixelsJS(jimpImage, imageOut, 36, 52, 32, 64, 48, 20, 52, 32);
-      imageOut = await copyRawPixelsJS(jimpImage, imageOut, 40, 52, 36, 64, 44, 20, 48, 32);
-      imageOut = await copyRawPixelsJS(jimpImage, imageOut, 44, 52, 40, 64, 40, 20, 44, 32);
-      imageOut = await copyRawPixelsJS(jimpImage, imageOut, 48, 52, 44, 64, 52, 20, 56, 32);
+      imageOut = await copyRawPixelsJS(
+        jimpImage,
+        imageOut,
+        24,
+        48,
+        20,
+        52,
+        4,
+        16,
+        8,
+        20,
+      );
+      imageOut = await copyRawPixelsJS(
+        jimpImage,
+        imageOut,
+        28,
+        48,
+        24,
+        52,
+        8,
+        16,
+        12,
+        20,
+      );
+      imageOut = await copyRawPixelsJS(
+        jimpImage,
+        imageOut,
+        20,
+        52,
+        16,
+        64,
+        8,
+        20,
+        12,
+        32,
+      );
+      imageOut = await copyRawPixelsJS(
+        jimpImage,
+        imageOut,
+        24,
+        52,
+        20,
+        64,
+        4,
+        20,
+        8,
+        32,
+      );
+      imageOut = await copyRawPixelsJS(
+        jimpImage,
+        imageOut,
+        28,
+        52,
+        24,
+        64,
+        0,
+        20,
+        4,
+        32,
+      );
+      imageOut = await copyRawPixelsJS(
+        jimpImage,
+        imageOut,
+        32,
+        52,
+        28,
+        64,
+        12,
+        20,
+        16,
+        32,
+      );
+      imageOut = await copyRawPixelsJS(
+        jimpImage,
+        imageOut,
+        40,
+        48,
+        36,
+        52,
+        44,
+        16,
+        48,
+        20,
+      );
+      imageOut = await copyRawPixelsJS(
+        jimpImage,
+        imageOut,
+        44,
+        48,
+        40,
+        52,
+        48,
+        16,
+        52,
+        20,
+      );
+      imageOut = await copyRawPixelsJS(
+        jimpImage,
+        imageOut,
+        36,
+        52,
+        32,
+        64,
+        48,
+        20,
+        52,
+        32,
+      );
+      imageOut = await copyRawPixelsJS(
+        jimpImage,
+        imageOut,
+        40,
+        52,
+        36,
+        64,
+        44,
+        20,
+        48,
+        32,
+      );
+      imageOut = await copyRawPixelsJS(
+        jimpImage,
+        imageOut,
+        44,
+        52,
+        40,
+        64,
+        40,
+        20,
+        44,
+        32,
+      );
+      imageOut = await copyRawPixelsJS(
+        jimpImage,
+        imageOut,
+        48,
+        52,
+        44,
+        64,
+        52,
+        20,
+        56,
+        32,
+      );
 
       jimpImage = imageOut;
     }
@@ -182,33 +432,187 @@ export namespace ImageEditor {
     return newBuff;
   }
 
-  export async function toEaglerSkin(image: Buffer): Promise<Util.BoundedBuffer<typeof Constants.EAGLERCRAFT_SKIN_CUSTOM_LENGTH>> {
+  export async function toEaglerSkin(
+    image: Buffer,
+  ): Promise<
+    Util.BoundedBuffer<typeof Constants.EAGLERCRAFT_SKIN_CUSTOM_LENGTH>
+  > {
     const meta = await sharp(image).metadata();
     let sharpImage = sharp(image);
     if (meta.height != 64) {
       // assume 32 height skin
-      let imageOut = sharp(await sharpImage.extend({ bottom: 32, background: { r: 0, g: 0, b: 0, alpha: 0 } }).toBuffer());
+      let imageOut = sharp(
+        await sharpImage
+          .extend({ bottom: 32, background: { r: 0, g: 0, b: 0, alpha: 0 } })
+          .toBuffer(),
+      );
 
-      imageOut = await copyRawPixels(sharpImage, imageOut, 24, 48, 20, 52, 4, 16, 8, 20);
-      imageOut = await copyRawPixels(sharpImage, imageOut, 28, 48, 24, 52, 8, 16, 12, 20);
-      imageOut = await copyRawPixels(sharpImage, imageOut, 20, 52, 16, 64, 8, 20, 12, 32);
-      imageOut = await copyRawPixels(sharpImage, imageOut, 24, 52, 20, 64, 4, 20, 8, 32);
-      imageOut = await copyRawPixels(sharpImage, imageOut, 28, 52, 24, 64, 0, 20, 4, 32);
-      imageOut = await copyRawPixels(sharpImage, imageOut, 32, 52, 28, 64, 12, 20, 16, 32);
-      imageOut = await copyRawPixels(sharpImage, imageOut, 40, 48, 36, 52, 44, 16, 48, 20);
-      imageOut = await copyRawPixels(sharpImage, imageOut, 44, 48, 40, 52, 48, 16, 52, 20);
-      imageOut = await copyRawPixels(sharpImage, imageOut, 36, 52, 32, 64, 48, 20, 52, 32);
-      imageOut = await copyRawPixels(sharpImage, imageOut, 40, 52, 36, 64, 44, 20, 48, 32);
-      imageOut = await copyRawPixels(sharpImage, imageOut, 44, 52, 40, 64, 40, 20, 44, 32);
-      imageOut = await copyRawPixels(sharpImage, imageOut, 48, 52, 44, 64, 52, 20, 56, 32);
+      imageOut = await copyRawPixels(
+        sharpImage,
+        imageOut,
+        24,
+        48,
+        20,
+        52,
+        4,
+        16,
+        8,
+        20,
+      );
+      imageOut = await copyRawPixels(
+        sharpImage,
+        imageOut,
+        28,
+        48,
+        24,
+        52,
+        8,
+        16,
+        12,
+        20,
+      );
+      imageOut = await copyRawPixels(
+        sharpImage,
+        imageOut,
+        20,
+        52,
+        16,
+        64,
+        8,
+        20,
+        12,
+        32,
+      );
+      imageOut = await copyRawPixels(
+        sharpImage,
+        imageOut,
+        24,
+        52,
+        20,
+        64,
+        4,
+        20,
+        8,
+        32,
+      );
+      imageOut = await copyRawPixels(
+        sharpImage,
+        imageOut,
+        28,
+        52,
+        24,
+        64,
+        0,
+        20,
+        4,
+        32,
+      );
+      imageOut = await copyRawPixels(
+        sharpImage,
+        imageOut,
+        32,
+        52,
+        28,
+        64,
+        12,
+        20,
+        16,
+        32,
+      );
+      imageOut = await copyRawPixels(
+        sharpImage,
+        imageOut,
+        40,
+        48,
+        36,
+        52,
+        44,
+        16,
+        48,
+        20,
+      );
+      imageOut = await copyRawPixels(
+        sharpImage,
+        imageOut,
+        44,
+        48,
+        40,
+        52,
+        48,
+        16,
+        52,
+        20,
+      );
+      imageOut = await copyRawPixels(
+        sharpImage,
+        imageOut,
+        36,
+        52,
+        32,
+        64,
+        48,
+        20,
+        52,
+        32,
+      );
+      imageOut = await copyRawPixels(
+        sharpImage,
+        imageOut,
+        40,
+        52,
+        36,
+        64,
+        44,
+        20,
+        48,
+        32,
+      );
+      imageOut = await copyRawPixels(
+        sharpImage,
+        imageOut,
+        44,
+        52,
+        40,
+        64,
+        40,
+        20,
+        44,
+        32,
+      );
+      imageOut = await copyRawPixels(
+        sharpImage,
+        imageOut,
+        48,
+        52,
+        44,
+        64,
+        52,
+        20,
+        56,
+        32,
+      );
 
       sharpImage = imageOut;
     }
 
-    const r = await sharpImage.extractChannel("red").raw({ depth: "uchar" }).toBuffer();
-    const g = await sharpImage.extractChannel("green").raw({ depth: "uchar" }).toBuffer();
-    const b = await sharpImage.extractChannel("blue").raw({ depth: "uchar" }).toBuffer();
-    const a = await sharpImage.ensureAlpha().extractChannel(3).toColorspace("b-w").raw({ depth: "uchar" }).toBuffer();
+    const r = await sharpImage
+      .extractChannel("red")
+      .raw({ depth: "uchar" })
+      .toBuffer();
+    const g = await sharpImage
+      .extractChannel("green")
+      .raw({ depth: "uchar" })
+      .toBuffer();
+    const b = await sharpImage
+      .extractChannel("blue")
+      .raw({ depth: "uchar" })
+      .toBuffer();
+    const a = await sharpImage
+      .ensureAlpha()
+      .extractChannel(3)
+      .toColorspace("b-w")
+      .raw({ depth: "uchar" })
+      .toBuffer();
     const newBuff = Buffer.alloc(Constants.EAGLERCRAFT_SKIN_CUSTOM_LENGTH);
     for (let i = 1; i < 64 ** 2; i++) {
       const bytePos = i * 4;
@@ -220,7 +624,9 @@ export namespace ImageEditor {
     return newBuff;
   }
 
-  export function generateEaglerMOTDImage(file: string | Buffer): Promise<Buffer> {
+  export function generateEaglerMOTDImage(
+    file: string | Buffer,
+  ): Promise<Buffer> {
     return new Promise<Buffer>((res, rej) => {
       sharp(file)
         .resize(Constants.ICON_SQRT, Constants.ICON_SQRT, {
@@ -242,12 +648,16 @@ export namespace ImageEditor {
     });
   }
 
-  export function generateEaglerMOTDImageJS(file: string | Buffer): Promise<Buffer> {
-    return new Promise<Buffer>(async (res, rej) => {
-      Jimp.read(typeof file == "string" ? await fs.readFile(file) : file, async (err, image) => {
-        image = image.resize(Constants.ICON_SQRT, Constants.ICON_SQRT, Jimp.RESIZE_NEAREST_NEIGHBOR);
-        res(image.bitmap.data);
-      });
+  export async function generateEaglerMOTDImageJS(
+    file: string | Buffer,
+  ): Promise<Buffer> {
+    const jimpImage = await Jimp.Jimp.read(
+      typeof file == "string" ? await fs.readFile(file) : file);
+    const resizedImage = jimpImage.resize({
+      w: Constants.ICON_SQRT,
+      h: Constants.ICON_SQRT,
+      strategy: Jimp.ResizeStrategy.RESIZE_NEAREST_NEIGHBOR
     });
+    return resizedImage.bitmap.data;
   }
 }
